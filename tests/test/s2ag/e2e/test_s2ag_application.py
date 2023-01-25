530d2c240e17591a36dc030b206d3af25caa2b3b
Romilly Cocking <romilly.cocking@gmail.com>
2023-01-25 12:20:16 +0000

    Web based tests now use vcrpy and run very fast.

--- tests/test/s2ag/e2e/test_s2ag_application.py
+++ tests/test/s2ag/e2e/test_s2ag_application.py
@@ -1,12 +1,19 @@
 import unittest
 
+import vcr
 from hamcrest import assert_that, starts_with
 
 from s2ag.librarian import Librarian, Researcher
 from s2ag.retriever import WebRetriever
 
+test_vcr = vcr.VCR(
+    cassette_library_dir='helpers/cassettes',
+    path_transformer=vcr.VCR.ensure_suffix('.yaml')
+)
 
 class S2AGTestCase(unittest.TestCase):
+
+    @test_vcr.use_cassette
     def test_librarian_retrieves_unknown_paper(self):
         retriever = WebRetriever()
         pid = '649def34f8be52c8b66281af98ae884c09aef38b'
--- tests/test/s2ag/integration/test_researcher.py
+++ tests/test/s2ag/integration/test_researcher.py
@@ -1,10 +1,18 @@
 import unittest
 
+import vcr
+
 from s2ag.researcher import Researcher, Retriever
 from s2ag.retriever import WebRetriever
 
+test_vcr = vcr.VCR(
+    cassette_library_dir='helpers/cassettes',
+    path_transformer=vcr.VCR.ensure_suffix('.yaml')
+)
 
 class ResearcherTestCase(unittest.TestCase):
+
+    @test_vcr.use_cassette
     def test_researcher_can_get_paper_using_api(self):
         pid = '649def34f8be52c8b66281af98ae884c09aef38b'
         researcher = Researcher(WebRetriever())
