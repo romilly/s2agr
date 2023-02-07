340130ff35f6f023f430d2662d0e4bd7371e74d3
Romilly Cocking <romilly.cocking@gmail.com>
2023-02-07 08:44:38 +0000

    Added mind map

---
+++ plan/Research-Use-cases.mm
@@ -0,0 +1,129 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Research&#xa;Use cases" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1675706613882"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#808080ff,#808080ff,#808080ff,#808080ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Researcher
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="S2AG" POSITION="right" ID="ID_1256999217" CREATED="1675706213179" MODIFIED="1675706662489">
+<edge COLOR="#808080"/>
+<node TEXT="Finds papers by keyword" ID="ID_15954422" CREATED="1675706218499" MODIFIED="1675706263275">
+<node TEXT="also area" ID="ID_1953292957" CREATED="1675706349497" MODIFIED="1675706361599"/>
+<node TEXT="year(s)" ID="ID_1262735922" CREATED="1675706362136" MODIFIED="1675706365831"/>
+</node>
+<node TEXT="Finds papers by author" ID="ID_1734437870" CREATED="1675706229715" MODIFIED="1675706274400"/>
+<node TEXT="Finds paper details" ID="ID_1637785806" CREATED="1675706275206" MODIFIED="1675706662488">
+<node TEXT="tldr" ID="ID_589452783" CREATED="1675706295546" MODIFIED="1675706300735"/>
+<node TEXT="embedding" ID="ID_361982422" CREATED="1675706301618" MODIFIED="1675706305238"/>
+<node TEXT="references" ID="ID_705883950" CREATED="1675706306825" MODIFIED="1675706312280"/>
+<node TEXT="citations" ID="ID_56470951" CREATED="1675706312713" MODIFIED="1675706323576"/>
+</node>
+<node TEXT="Ranks papers by relevance" ID="ID_1204236981" CREATED="1675706387487" MODIFIED="1675706400911"/>
+<node TEXT="Finds similar papers" ID="ID_515483471" CREATED="1675706467431" MODIFIED="1675706478069"/>
+<node TEXT="Adds paper to logseq" ID="ID_511586000" CREATED="1675706553550" MODIFIED="1675706562093">
+<node TEXT="with wikipwedia reference" ID="ID_1471837262" CREATED="1675706568686" MODIFIED="1675706583035"/>
+</node>
+<node TEXT="Finds GitHub references" ID="ID_406571546" CREATED="1675706667060" MODIFIED="1675706679778"/>
+</node>
+<node TEXT="Archives" POSITION="left" ID="ID_1866348786" CREATED="1675706421504" MODIFIED="1675706518243">
+<edge COLOR="#808080"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Arxiv, bioarxiv etc.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Finds recent papers" ID="ID_102621341" CREATED="1675706427536" MODIFIED="1675706437773">
+<node TEXT="by topic" ID="ID_769290137" CREATED="1675706438839" MODIFIED="1675706447606"/>
+<node TEXT="by author" ID="ID_1616731676" CREATED="1675706531670" MODIFIED="1675706535604"/>
+</node>
+</node>
+<node TEXT="Books" POSITION="right" ID="ID_1012384452" CREATED="1675706593293" MODIFIED="1675706597699">
+<edge COLOR="#808080"/>
+<node TEXT="Kindle" ID="ID_458530" CREATED="1675706623637" MODIFIED="1675706626195">
+<node TEXT="Finds referenced papers" ID="ID_400361647" CREATED="1675706627493" MODIFIED="1675706655266"/>
+</node>
+<node TEXT="Finds catalogued books" ID="ID_73186186" CREATED="1675706688732" MODIFIED="1675706721513"/>
+<node TEXT="Saves highlightsd to logseq" ID="ID_1904130504" CREATED="1675706703284" MODIFIED="1675706715418"/>
+</node>
+<node TEXT="logseq" POSITION="left" ID="ID_725600654" CREATED="1675706735891" MODIFIED="1675706739089">
+<edge COLOR="#808080"/>
+<node TEXT="build topic glossary" ID="ID_903570722" CREATED="1675706740483" MODIFIED="1675706770896">
+<node TEXT="linked" ID="ID_370100646" CREATED="1675706772691" MODIFIED="1675706777577"/>
+<node TEXT="tagged papers" ID="ID_1765611506" CREATED="1675706778659" MODIFIED="1675706797161"/>
+</node>
+</node>
+</node>
+</map>
