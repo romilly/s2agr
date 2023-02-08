30f9e1402bc33a1a606076a0b5d71478a871f2dd
Romilly Cocking <romilly.cocking@gmail.com>
2023-02-08 12:39:46 +0000

    11th baby step as I refactor Paginator, researcher etc/

--- src/s2ag/urls.py
+++ src/s2ag/urls.py
@@ -17,10 +17,6 @@ class UrlBuilder(ABC):
         items = self.query.parameters().items()
         return '' if len(items) == 0 else '?'+'&'.join(f'{key}={value}' for (key, value) in items)
 
-    # def for_citations_of(self, paper_id: str):
-    #     query_part = self.get_query_string()
-    #     return f'{self.BASE_URL}paper/{paper_id}/citations{query_part}'
-
     @abstractmethod
     def get_url(self):
         pass
@@ -28,8 +24,7 @@ class UrlBuilder(ABC):
 
 class UrlBuilderForSearch(UrlBuilder):
     def get_url(self):
-        query_part = self.get_query_string()
-        return f'{self.BASE_URL}paper/search{query_part}'
+        return f'{self.BASE_URL}paper/search{self.get_query_string()}'
 
 
 class UrlBuilderForSinglePaper(UrlBuilder):
@@ -38,8 +33,7 @@ class UrlBuilderForSinglePaper(UrlBuilder):
         self.paper_id = paper_id
 
     def get_url(self):
-        query_part = self.get_query_string()
-        return f'{self.BASE_URL}paper/{self.paper_id}{query_part}'
+        return f'{self.BASE_URL}paper/{self.paper_id}{self.get_query_string()}'
 
 
 class UrlBuilderForPaperCitations(UrlBuilder):
@@ -48,8 +42,8 @@ class UrlBuilderForPaperCitations(UrlBuilder):
         self.paper_id = paper_id
 
     def get_url(self):
-        query_part = self.get_query_string()
-        return f'{self.BASE_URL}paper/{self.paper_id}/citations{query_part}'
+        return f'{self.BASE_URL}paper/{self.paper_id}/citations{self.get_query_string()}'
+
 
 class UrlBuilderForAuthor(UrlBuilder):
     def __init__(self, author_id: str):
@@ -57,13 +51,4 @@ class UrlBuilderForAuthor(UrlBuilder):
         self.author_id = author_id
 
     def get_url(self):
-        query_part = self.get_query_string()
-        return f'{self.BASE_URL}author/{self.author_id}{query_part}'
-
-
-
-
-
-
-
-
+        return f'{self.BASE_URL}author/{self.author_id}{self.get_query_string()}'
