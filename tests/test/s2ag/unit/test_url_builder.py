df9ec2d3a849e33a36f85bd57a81d994cfef95d4
Romilly Cocking <romilly.cocking@gmail.com>
2023-02-08 08:50:41 +0000

    Refactored UrlBuilder test

--- tests/test/s2ag/unit/test_url_builder.py
+++ tests/test/s2ag/unit/test_url_builder.py
@@ -2,19 +2,24 @@ import unittest
 
 from s2ag.urls import UrlBuilder
 
-
+# TODO: add more examples
 class UrlBuilderTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.ub = UrlBuilder()
+
     def test_builds_url_for_search(self):
-        ub = UrlBuilder()
-        url = ub.for_search({'query': 'cerebellar+cortex'})
+        url = self.ub.for_search({'query': 'cerebellar+cortex'})
         expected_url = 'https://api.semanticscholar.org/graph/v1/paper/search?query=cerebellar+cortex'
         self.assertEqual(expected_url, url)
 
+
+
     def test_builds_url_for_paper(self):
-        ub = UrlBuilder()
-        url = ub.for_paper('649def34f8be52c8b66281af98ae884c09aef38b')
+        actual_url = self.ub.for_paper('649def34f8be52c8b66281af98ae884c09aef38b')
         expected_url = 'https://api.semanticscholar.org/graph/v1/paper/649def34f8be52c8b66281af98ae884c09aef38b'
-        self.assertEqual(expected_url, url)
+        self.assertEqual(expected_url, actual_url)
+
+
 
 
 
